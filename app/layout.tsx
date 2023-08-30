c22c1e131ba17eb1a4db390e6bf166d69c3e801b
Devansh-365 <devanshtiwari365@gmail.com>
2023-08-30 17:11:49 +0530

    feat: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Inter, Open_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { ToastProvider } from "@/components/providers/toast-provider";
+import { Analytics } from "@vercel/analytics/react";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -28,6 +29,7 @@ export default function RootLayout({
         >
           <ToastProvider />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
